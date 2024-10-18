815e85176787085c7220d2161349b1eb5318a234
Harsh Jain <113963702+harshjain0@users.noreply.github.com>
2024-10-18 16:42:32 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+            Hello, Welcome to my Blog
+        </h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident natus voluptas totam at voluptatum nisi deserunt ut. Atque ab aut sequi obcaecati sint voluptas magnam laborum architecto doloribus ullam iusto, corrupti molestias, laboriosam iste laudantium a unde quisquam, ea incidunt sit totam est? Impedit sed recusandae eligendi accusantium cupiditate.
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab at excepturi dolor eaque tempore a vitae repellendus. Minus, nemo fugit harum sunt doloribus ipsam necessitatibus quaerat eaque libero eveniet perspiciatis officiis, ea nulla nobis temporibus similique numquam. Animi doloribus officiis atque maxime ipsum autem ipsam natus quam, architecto voluptas laudantium.
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto unde sed repellat beatae repellendus ad, ullam et, asperiores laborum non necessitatibus doloribus veritatis optio libero quibusdam commodi. Enim fugit quas libero quo optio dolore magni reprehenderit, doloribus fuga at, esse nobis adipisci iste numquam dolorum consectetur totam porro. Blanditiis numquam facere, quibusdam odit, accusamus magnam possimus quae consectetur aliquam dolor assumenda molestiae eum quod earum voluptatem temporibus. At quis error non vero id officiis, magni quia eveniet architecto quod minus obcaecati facere commodi, numquam odio similique sequi reiciendis laboriosam eligendi, minima vitae explicabo deleniti nulla. Culpa vitae minus quaerat ullam!
+        </p>
+    </div>
+  )
+}
+
+export default Home
